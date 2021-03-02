51b0152949a57a3f90e776d15933d442d8d45e8c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-03-02 17:40:31 +0000

    visual-studio.ps1: use visualstudio2019community

--- visual-studio.ps1
+++ visual-studio.ps1
@@ -5,7 +5,7 @@
 
 iwr -useb cin.st | iex
 choco feature enable --name=allowGlobalConfirmation
-cinst visualstudio2019professional visualstudio2019-workload-manageddesktop netfx-4.8-devpack
+cinst visualstudio2019community visualstudio2019-workload-manageddesktop netfx-4.8-devpack
 cinst resharper-ultimate-all ncrunch-vs2019
 cinst nuget.commandline
 
